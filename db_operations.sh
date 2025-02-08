bbbd52b07eefc8f0deed84a6812dfbfab5acd5d2
marriaammm <mariamz@std.mans.edu.eg>
2025-02-08 17:21:58 +0200

    dp_operations updated

--- db_operations.sh
+++ db_operations.sh
@@ -3,43 +3,40 @@
 DB_DIR="./db_storage"
 
 if [[ ! -d "$DB_DIR" ]]; then
-mkdir -p "$DB_DIR"
+    mkdir -p "$DB_DIR"
 fi
 
-create_database(){
-read -p "Enter database name: " db_name
-if [[ -d "$DB_DIR/$db_name" ]]; then
-   echo "Database '$db_name' already exists."
-else
-   mkdir "$DB_DIR/$db_name"
-   echo "Database '$db_name' created successfully."
-fi
+create_database() {
+    read -p "Enter database name: " db_name
+    if [[ -d "$DB_DIR/$db_name" ]]; then
+        echo "Database '$db_name' already exists."
+    else
+        mkdir "$DB_DIR/$db_name"
+        echo "Database '$db_name' created successfully."
+    fi
 }
 
-
-list_databases(){
-echo "Available databases: "
-ls "$DB_DIR"
+list_databases() {
+    echo "Available databases:"
+    ls "$DB_DIR"
 }
 
-
-drop_database(){
-read -p "Enter database name to drop: " db_name
-if [[ -d "$DB_DIR/$db_name" ]]; then
-   rm -r "$DB_DIR/$db_name"
-   echo "Database '$db_name' deleted."
-else
-   echo "Database '$db_name' does not exist."
-fi
+connect_database() {
+    read -p "Enter database name to connect: " db_name
+    if [[ -d "$DB_DIR/$db_name" ]]; then
+        echo "Connected to database '$db_name'."
+        table_menu "$db_name"
+    else
+        echo "Database '$db_name' does not exist."
+    fi
 }
 
-
-connect_database(){
-read -p "Enter database name to connect: "db_name
-if [[ -d "$DB_DIR/%db_name" ]]; then
-   echo "Connected to database '$db_name'."
-   ./table_operations.sh "$db_name"
-else
-   echo "Database '$db_name' does not exist."
-fi
+drop_database() {
+    read -p "Enter database name to drop: " db_name
+    if [[ -d "$DB_DIR/$db_name" ]]; then
+        rm -r "$DB_DIR/$db_name"
+        echo "Database '$db_name' deleted."
+    else
+        echo "Database '$db_name' does not exist."
+    fi
 }
